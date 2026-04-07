848aab23b573a16be1f3c7066a9a314723fd12b2
Mathias <4mathias.kodehode@gmail.com>
2026-04-07 14:26:09 +0200

    add initial html structure

--- src/app.js
+++ src/app.js
@@ -1,3 +1,41 @@
+import { CreateElement } from "./helperFunctions.js";
+import { main } from "./helperFunctions.js";
+import { testimonials } from "./testimonial.js";
+
 export function Init() {
-    console.log("hello, world!");
+    const container = CreateElement("div", {
+        parent: main,
+    });
+    testimonials.forEach(t => {
+        const testimonialContainer = CreateElement("div", {
+            parent: container,
+            class: "testimonialContainer",
+        });
+        // profile picture
+        CreateElement("img", {
+            parent: testimonialContainer,
+            src: t.imageSrc,
+            alt: "portrait of reivewer",
+        });
+        // name
+        CreateElement("h2", {
+            parent: testimonialContainer,
+            innerText: t.userName,
+        });
+        // title
+        CreateElement("h3", {
+            parent: testimonialContainer,
+            innerText: t.userTitle,
+        });
+        // content title
+        CreateElement("h1", {
+            parent: testimonialContainer,
+            innerText: t.contentTitle,
+        });
+        // content body
+        CreateElement("p", {
+            parent: testimonialContainer,
+            innerText: t.contentBody,
+        });
+    });
 }
